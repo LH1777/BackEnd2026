2756f8cf62f109697f0aefcfbf556c107119ae9c
LH1777 <rabelloluis25@gmail.com>
2026-02-11 21:18:26 -0300

    Primeiro código do curso de C#

---
+++ Senai 2026.cs
@@ -0,0 +1,8 @@
+using System; //Biblioteca de classes do C#(system) para usar comandos do Console
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Senai 2026"); //Comando para escrever no console
+    }
+}
---
+++ a.cs
@@ -0,0 +1,1 @@
+Console.WriteLine("Senai 2026");
