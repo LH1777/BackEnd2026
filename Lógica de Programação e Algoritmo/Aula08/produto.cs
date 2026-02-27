a24691419ba309a3e199e0842c0a7722ddcad2f6
LH1777 <rabelloluis25@gmail.com>
2026-02-27 20:56:28 -0300

    produto

---
+++ Lógica de Programação e Algoritmo/Aula08/produto.cs
@@ -0,0 +1,27 @@
+using static System.Console;
+
+//------------------Declaração de variáveis------------
+int codigo, quantidade;
+//------------------Entrada de dados-------------------
+Write("Digite o código do produto: ")
+codigo = int.Parse(ReadLine());
+Write("Digite a quantidade de produto: ")
+quantidade = int.Parse(ReadLine());
+switch (codigo)
+{
+    case 1:
+        WriteLine($"O valor total é de: {quantidade * 5.0}");
+        break;
+    case 2:
+        WriteLine($"O valor total é de: {quantidade * 4.0}");
+        break;
+    case 3:
+        WriteLine($"O valor total é de: {quantidade * 3.75}");
+        break;
+    case 4:
+        WriteLine($"O valor total é de: {quantidade * 8.99}");
+        break;
+    case 5:
+        WriteLine($"O valor total é de: {quantidade * 11.33}");
+        break;
+}
