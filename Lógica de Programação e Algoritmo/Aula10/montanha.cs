53a47307014e42c8b44282c90fe1dc7f31a5be6a
LH1777 <rabelloluis25@gmail.com>
2026-06-23 18:59:29 -0300

    montanha russa atividade 02

--- Lógica de Programação e Algoritmo/Aula10/montanha.cs
+++ Lógica de Programação e Algoritmo/Aula10/montanha.cs
@@ -1,31 +1,35 @@
 using static System.Console;
 using static System.Linq.Enumerable;
 
-//Declaração de variaveis
 int visitantes = 0, contagem = 0;
 double altura_max = 0, altura_min = 0, alturas = 0;
-try{
-    //Enrtada de dados
+
+try
+{
     Write("Digite o numero de visitantes: ");
     visitantes = int.Parse(ReadLine());
+
     Write("Digite a altura maxima: ");
-    alturas_max = double.Parse(ReadLine());
+    altura_max = double.Parse(ReadLine());
+
     Write("Digite a altura minima: ");
     altura_min = double.Parse(ReadLine());
-    //Processamento de dados
+
     foreach(int i in Range(0, visitantes))
     {
         Write($"Digite a altura da pessoa nº {i + 1}: ");
-        alturas = int.Parse(ReadLine());
-        if ((alturas >= altura_min) & (alturas <= altura_max))
+        alturas = double.Parse(ReadLine());
+
+        if ((alturas >= altura_min) && (alturas <= altura_max))
         {
             contagem++;
-            WriteLine("Pessoa pode entrar no parque");
+            WriteLine("Pessoa pode andar na montanha-russa");
         }
     }
-    WriteLine($"O número de pessoas que podem entrar no parque é de {contagem}");
+
+    WriteLine($"O número de pessoas que podem andar é {contagem}");
 }
 catch (Exception ex)
 {
-    WriteLine($"Valor invalido, digite numerais, erro = {ex.Message}");
+    WriteLine($"Valor invalido, erro = {ex.Message}");
 }
